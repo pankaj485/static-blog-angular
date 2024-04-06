d80edb8eb0750ca751d3880da722603870013454
pankaj485 <pankajsamar2@gmail.com>
2024-04-06 19:29:43 +0530

    add routing for blogs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { BlogsComponent } from './blogs/blogs.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, BlogsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'blog-sample';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { BlogsComponent } from './blogs/blogs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'blogs', component: BlogsComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'blogs' },
+  { path: '**', redirectTo: 'blogs' },
+];
